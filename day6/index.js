5d980f62e0b9f69378cf7730eda5cc6d1d66ee44
Mikael Olausson <mikaeo@usit.uio.no>
2019-12-06 08:07:41 +0100

    init day6 with solution

---
+++ day6/index.js
@@ -0,0 +1,31 @@
+import * as fs from "fs";
+import pngjs from "pngjs";
+
+const getPngIndex = (imageWidth, x, y) => (imageWidth * y + x) << 2;
+
+fs.createReadStream("./mush.png")
+  .pipe(
+    new pngjs.PNG({
+      filterType: 4,
+    }),
+  )
+  .on("parsed", function() {
+    let current = this.data[
+      getPngIndex(this.width, this.width - 1, this.height - 1)
+    ];
+
+    for (let y = this.height - 2; y > 0; y--) {
+      for (let x = this.width - 2; x > 0; x--) {
+        let prev = getPngIndex(this.width, x, y);
+
+        // xor withprev
+        this.data[current] = this.data[current] ^ this.data[prev];
+        this.data[current + 1] = this.data[current + 1] ^ this.data[prev + 1];
+        this.data[current + 2] = this.data[current + 2] ^ this.data[prev + 2];
+
+        current = prev;
+      }
+    }
+
+    this.pack().pipe(fs.createWriteStream("knowit-day6.png"));
+  });
